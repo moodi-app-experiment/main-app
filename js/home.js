8cf7c90454522ab91654b2e5f886264d51455325
moodi-app-experiment <moodi.admin@gmail.com>
2026-03-24 23:44:14 +1100

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (!target) return;
+    e.preventDefault();
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+});
