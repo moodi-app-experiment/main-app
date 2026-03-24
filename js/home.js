83de744f023d08a7108f3e66d8cfb8f7aeb0ffaa
moodi-app-experiment <moodi.admin@gmail.com>
2026-03-24 23:50:05 +1100

    Delete js directory

--- js/home.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    const target = document.querySelector(this.getAttribute('href'));
-    if (!target) return;
-    e.preventDefault();
-    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  });
-});
